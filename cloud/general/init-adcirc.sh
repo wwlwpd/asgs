9fed89388e56867e5e8580ff43060bd2e6e581f9
wwlwpd <46434714+wwlwpd@users.noreply.github.com>
2019-08-28 10:16:34 -0500

    Update init-adcirc.sh

--- cloud/general/init-adcirc.sh
+++ cloud/general/init-adcirc.sh
@@ -18,6 +18,8 @@ export LD_LIBRARY_PATH=/home/vagrant/opt/lib
 export PERLBREW_PATH=/home/vagrant/perl5/perlbrew/bin:/home/vagrant/perl5/perlbrew/perls/perl-5.28.2/bin
 export LIBRARY_PATH=/home/vagrant/opt/lib
 
+cd $HOME/adcirc-cg
+git checkout v53release
 cd $HOME/adcirc-cg/work
 make clean clobber
 make all adcswan padcswan compiler=gfortran NETCDF=enable NETCDF4=enable NETCDF4_COMPRESSION=enable NETCDFHOME=$NETCDFHOME MACHINENAME=jason-desktop
