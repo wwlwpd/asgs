8e474a38cf1e5ea688cd99d0c8eed1adaa7603a6
user <user@computer.local>
2019-08-28 23:46:23 -0500

    source perlbrew in perl module installer

--- cloud/general/init-perl-modules.sh
+++ cloud/general/init-perl-modules.sh
@@ -3,6 +3,8 @@
 # unconditionally attempts to install the Perl modules that are required
 #for the operation of ASGS.
 
+source ~/perl5/perlbrew/etc/bashrc
+
 perlbrew --force install-cpanm
 cpanm --local-lib=~/perl5 local::lib && eval $(perl -I ~/perl5/lib/perl5/ -Mlocal::lib)
 
